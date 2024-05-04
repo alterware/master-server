a04a6b758d32fcab4f2abf12b4af2d0cc7c97ef9
Future <edoardo.sanguineti222@gmail.com>
2024-05-04 10:00:34 +0200

    fix: re-write patch

--- src/server_list.hpp
+++ src/server_list.hpp
@@ -3,7 +3,7 @@
 #include "game_server.hpp"
 #include "network_list.hpp"
 
-class server_list : public network_list<game_server, 30>
+class server_list : public network_list<game_server, 20>
 {
 public:
 	void find_registered_servers(game_type game, int protocol, const access_func& accessor);
